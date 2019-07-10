85400c10701d75a02a4e164444f08b2ecf4130c1
JoaoVitorMalvestio <joaovitormalvestio@gmail.com>
2019-07-10 17:14:57 -0300

    Adicionado divido e criado 4 novos componentes, iniciado e concluido a view dos componente top-bar e character-search, iniciado o desenvolvimento da view da character-list, criado também a classe do character

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CharacterSearchComponent } from './character-search/character-search.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterDetailsComponent } from './character-details/character-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      TopBarComponent,
+      CharacterSearchComponent,
+      CharacterListComponent,
+      CharacterDetailsComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
