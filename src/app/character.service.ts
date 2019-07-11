cda002875e2a6f6359d9887fb60c8e58ad3c68be
JoaoVitorMalvestio <joaovitormalvestio@gmail.com>
2019-07-11 16:57:32 -0300

    Corrigido o getCharacters, carregando a tabela com a foto nome e descricao do personagem

--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -17,14 +17,14 @@ export class CharacterService {
   getCharacters(term: string) {
     const filtro = term != ''?('?filter[name]=' + term): '';
 
-    return this.httpClient.get(this.baseUrl + '/characters' + filtro); /* .pipe(map((response: {data: any[]}) => {
-      return (response.data).map((character) => {
+    return this.httpClient.get(this.baseUrl + '/characters' + filtro).pipe(map((response: {data: Character[]}) => {
+      return (response.data); /*.map((character) => {
         return {
           id: character.id,
           description: character.attributes.description,
           name: character.attributes.name
         };
-      });
-    }));*/
+      });*/
+    }));
   }
 }
