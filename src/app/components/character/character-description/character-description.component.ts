d31730f89726d2437f734765154a24efb1c3d2e4
JoaoVitorMalvestio <joaovitormalvestio@gmail.com>
2019-07-17 14:04:54 -0300

    Retirado nome da internationalisation e melhorado componente de descricao do character

--- src/app/components/character/character-description/character-description.component.ts
+++ src/app/components/character/character-description/character-description.component.ts
@@ -7,16 +7,35 @@ import { Character } from '../character';
   styleUrls: ['./character-description.component.css']
 })
 export class CharacterDescriptionComponent implements OnInit {
-  @Input() character: Character;
+  private internalDescription: string;
+  public descriptionFormated: string;
+
+  get description() {
+    return this.internalDescription;
+  }
+
+  @Input() set description(value) {
+    this.internalDescription = value;
+
+    this.formatDescription();
+  }
+
+  formatDescription(){
+    this.descriptionFormated = this.internalDescription;    
+    this.descriptionFormated = this.tiraTagBr(this.descriptionFormated);
+    if (this.descriptionFormated.length > 200) { this.descriptionFormated =  this.descriptionFormated.slice(0, 200) + '...'; }
+  }
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
-
-  tiraTagBr(texto: string){
+  tiraTagBr(texto: string) {
     // Tira a tag <br> do texto
     return texto = texto.replace(/<br>/g, ' ');
   }
+
+
 }
